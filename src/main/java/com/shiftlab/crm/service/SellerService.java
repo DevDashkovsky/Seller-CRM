b93edcd13a38d70dc2fdbab85ad99eb0860d2242
Egor Dashkovskii <e.dashkovskii1@g.nsu.ru>
2026-05-17 14:44:02 +0700

    feat(service): add SellerService

---
+++ src/main/java/com/shiftlab/crm/service/SellerService.java
@@ -0,0 +1,43 @@
+package com.shiftlab.crm.service;
+
+
+import com.shiftlab.crm.dto.request.SellerCreateRequest;
+import com.shiftlab.crm.dto.response.SellerResponse;
+import com.shiftlab.crm.entity.Seller;
+import com.shiftlab.crm.mapper.SellerMapper;
+import com.shiftlab.crm.repository.SellerRepository;
+import jakarta.persistence.EntityNotFoundException;
+import java.time.LocalDateTime;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class SellerService {
+    private final SellerRepository repository;
+    private final SellerMapper mapper;
+
+    public SellerService(SellerRepository repository, SellerMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    @Transactional
+    public SellerResponse create(SellerCreateRequest request) {
+        Seller seller = mapper.toEntity(request);
+        if (seller.getRegistrationDate() == null) {
+            seller.setRegistrationDate(LocalDateTime.now());
+        }
+        Seller saved = repository.save(seller);
+        return mapper.toResponse(saved);
+    }
+
+    public SellerResponse getById(Long id) {
+        Seller seller = repository.findById(id).orElseThrow(
+            () -> new EntityNotFoundException(
+                "Seller with id %d not found".formatted(id)
+            )
+        );
+        return mapper.toResponse(seller);
+    }
+}
